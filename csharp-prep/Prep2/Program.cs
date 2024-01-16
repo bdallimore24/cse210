1ca04146d2c5007b6032de07dfc6b2943c54e2c8
bdallimore24 <bdallimore24@gmail.com>
2024-01-15 20:47:15 -0700

    week1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+
+            grade = "A";
+
+        }
+
+        else if (percent >= 80)
+        { 
+            
+            grade = "B";
+
+        }
+
+        else if (percent >= 70)
+        {
+
+            grade = "C";
+            
+        }
+
+        else if (percent >= 60)
+        {
+
+            grade = "D";
+
+        }
+
+        else
+        {
+
+            grade = "F";
+
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Oh good, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Well. . . Better luck next time!");
+        }
+
     }
 }
