e235e0fbfdcc2d02f5994295e30045c9ce7331c6
bdallimore24 <bdallimore24@gmail.com>
2024-02-06 14:23:48 -0700

    wk5prep

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square s1 = new Square("Purple", 5);
+        shapes.Add(s1);
+
+
+        Rectangle s2 = new Rectangle("Green", 5, 2);
+        shapes.Add(s2);
+
+
+        Circle s3 = new Circle("Red", 4);
+        shapes.Add(s3);
+
+
+        foreach (Shape shape in shapes)
+        {
+
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"Color: {color}. Area: {area}");
+            
+        }
+
     }
 }
